831b3927e1a030aec000a55bdfd6bedcebb559f6
swati <swatibinplus@gmail.com>
2025-05-01 10:25:40 +0530

    privacy api -done

--- app/src/main/java/com/cabbooking/activity/PrivacyPolicyActivity.java
+++ app/src/main/java/com/cabbooking/activity/PrivacyPolicyActivity.java
@@ -7,21 +7,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.cabbooking.R;
 import com.cabbooking.databinding.ActivityPrivacyPolicyBinding;
+import com.cabbooking.model.AppSettingModel;
+import com.cabbooking.utils.Common;
+import com.cabbooking.utils.OnConfig;
+import com.google.android.gms.common.api.CommonStatusCodes;
 
 public class PrivacyPolicyActivity extends AppCompatActivity {
     ActivityPrivacyPolicyBinding binding;
+    String type="";
+    Common common;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          binding = ActivityPrivacyPolicyBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        common=new Common(this);
+        type=getIntent().getStringExtra("type");
+        common.getAppSettingData(new OnConfig() {
+            @Override
+            public void getAppSettingData(AppSettingModel model) {
+                if (type.equals("terms")){
+                    binding.tvHeading.setText(getResources().getString(R.string.terms));
+                    binding.tvDescription.setText(model.getTerms_conditions());
 
-        if (getIntent().getStringExtra("type").equals("terms")){
-            binding.tvHeading.setText(getResources().getString(R.string.terms));
-        }else {
-            binding.tvHeading.setText(getResources().getString(R.string.privacy_policy));
-        }
+                }else {
+                    binding.tvHeading.setText(getResources().getString(R.string.privacy_policy));
+                    binding.tvDescription.setText(model.getPrivacy_policy());
+                }
+            }
+        });
+
 
         binding.incToolbar.ivBackarrow.setOnClickListener(new View.OnClickListener() {
             @Override
